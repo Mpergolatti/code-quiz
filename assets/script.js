668d488323b86620c5e6a0133cd9daec1fcdd7bf
Mpergolatti <Mpergolatti@gmail.com>
2022-09-23 10:22:15 -0400

    timer issues

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,44 @@
-var timerEl = document.getElementById('timer');
+// var timerEl = document.getElementById('countdown');
 
-// Time that counts down from 60
-function timer () {
-  var timeLeft = 60;
+// // Time that counts down from 60
+// function timer() {
+//   var timeLeft = 60;
 
-  // Using the setInterval() method to call function @ 1000ms
-  var timerCountdown = setInterval(function () {
+//   // Using the setInterval() method to call function @ 1000ms
+//   var timerCountdown = setInterval(function () {
 
-    if(timeLeft > 1) {
-      timerEl.textContent = timeLeft + ' Seconds Remaining';
-      timeLeft --;
+//     if(timeLeft > 1) {
+//       timerEl.textContent = timeLeft + ' Seconds Remaining';
+//       timeLeft --;
 
-    } else if (timeLeft === 1) {
-      timerEl.textContent = timeLeft + ' Second Remaining';
-      timeLeft --;
+//     } else if (timeLeft === 1) {
+//       timerEl.textContent = timeLeft + ' Second Remaining';
+//       timeLeft --;
 
-    } else {
-      timerEl.textContent = '';
-      clearInterval(timerCountdown);
-      displayMessage();
+//     } else {
+//       timerEl.textContent = '';
+//       clearInterval(timerCountdown);
+//       displayMessage();
+//     }
+
+//   }, 1000);
+  
+// }
+var timerEl = document.getElementById('some_div');
+
+var timeLeft = 5;
+    
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft == -1) {
+        clearTimeout(timerId);
+        doSomething();
+        
+      } else {
+        timerEl.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+
+        window.alert('Times Up!')
+      }
     }
-
-  }, 1000);
-  console.log(timer)
-}
-
