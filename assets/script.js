27aee3b06e013ea9bd6958b6360e8295f35d1d2e
Mpergolatti <Mpergolatti@gmail.com>
2022-09-26 19:07:56 -0400

    commit before updating vsCode

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,29 @@
-// var timerEl = document.getElementById('countdown');
+var timerEl = document.getElementById('countdown');
 
-// // Time that counts down from 60
-// function timer() {
-//   var timeLeft = 60;
+// Time that counts down from 60
+function timer() {
+  var timeLeft = 60;
 
-//   // Using the setInterval() method to call function @ 1000ms
-//   var timerCountdown = setInterval(function () {
+  // Using the setInterval() method to call function @ 1000ms
+  var timerCountdown = setInterval(function () {
 
-//     if(timeLeft > 1) {
-//       timerEl.textContent = timeLeft + ' Seconds Remaining';
-//       timeLeft --;
+    if(timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' Seconds Remaining';
+      timeLeft --;
 
-//     } else if (timeLeft === 1) {
-//       timerEl.textContent = timeLeft + ' Second Remaining';
-//       timeLeft --;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' Second Remaining';
+      // window.alert('Times Up');
+      timeLeft --;
 
-//     } else {
-//       timerEl.textContent = '';
-//       clearInterval(timerCountdown);
-//       displayMessage();
-//     }
-
-//   }, 1000);
-  
-// }
-var timerEl = document.getElementById('some_div');
-
-var timeLeft = 5;
-    
-    var timerId = setInterval(countdown, 1000);
-    
-    function countdown() {
-      if (timeLeft == -1) {
-        clearTimeout(timerId);
-        doSomething();
-        
-      } else {
-        timerEl.innerHTML = timeLeft + ' seconds remaining';
-        timeLeft--;
-
-        window.alert('Times Up!')
-      }
+    } else {
+      timerEl.textContent = 'Times Up!';
+      clearInterval(timerCountdown);
+      // window.alert('Times Up!');
     }
+
+  }, 1000);
+  
+}
+
+timer()
