64e915091edfbade200b648dc6c2836fdab829a4
Mpergolatti <Mpergolatti@gmail.com>
2022-12-04 09:42:52 -0500

    added question panel

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+
+
+// Questions
+
+var questionPanel = [
+  {
+    question: 'Question 1',
+    choices: [
+      'A. One',
+      'B. Two',
+      'C. Three',
+      'D. Four'
+    ],
+    answer: 'A'
+  },
+  {
+    question: 'Question 2',
+    choices: [
+      'A. One',
+      'B. Two',
+      'C. Three',
+      'D. Four'
+    ],
+    answer: 'A'
+  },
+  {
+    question: 'Question 3',
+    choices: [
+      'A. One',
+      'B. Two',
+      'C. Three',
+      'D. Four'
+    ],
+    answer: 'A'
+  },
+  {
+    question: 'Question 4',
+    choices: [
+      'A. One',
+      'B. Two',
+      'C. Three',
+      'D. Four'
+    ],
+    answer: 'A'
+  },
+  {
+    question: 'Question 5',
+    choices: [
+      'A. One',
+      'B. Two',
+      'C. Three',
+      'D. Four'
+    ],
+    answer: 'A'
+  },
+]
+
